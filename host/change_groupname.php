86f2139f13cfa544a451fa6e61ccc8ccdc0fb4c6
c0decaps <manuel.weiss@protonmail.ch>
2023-07-30 02:04:48 +0200

    Migrated socket and group name management to a sqlite db & some code sanitation

--- host/change_groupname.php
+++ host/change_groupname.php
@@ -1,12 +1,16 @@
 <?php
-include 'functions.php';
 
+$db = new SQLite3('data/rf.db');
 if(isset($_POST["new_groupname"]) && isset($_POST["group_to_change"])) {
-	$names_config_file = "data/config/names.ini";
-	$group_num = bindec($_POST["group_to_change"]) + 1;	
-	config_set($names_config_file, "group_names", "group".$group_num, $_POST["new_groupname"]);	
-	echo "config file ".$names_config_file." should now show ".bindec($_POST["group_to_change"])." as ".$_POST["new_groupname"];
-	header('Location: http://'.$_SERVER['HTTP_HOST'].'/');
+	$query = "UPDATE groups SET group_name='".$_POST["new_groupname"]."' WHERE group_id='".bindec($_POST["group_to_change"])."'";
+	echo $query;
+	$res = $db->exec($query);
+	if ($res = true) {
+		echo "<br> Query executed.";
+		header('Location: http://'.$_SERVER['HTTP_HOST'].'/');
+	} else {
+		echo "<br> Query not executed";
+	}
 } else {
 	echo "no groupname set";
 	header('Location: http://'.$_SERVER['HTTP_HOST'].'/');
